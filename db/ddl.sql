43db802f4342fc51a3e4d89486846cbec8a5f3ce
Allan Fernandes <allan54099976@aluno.ms.senac.br>
2025-03-25 21:39:49 -0400

    Realizando alterações no projeto

---
+++ db/ddl.sql
@@ -0,0 +1,24 @@
+create database  Devmedia;
+
+create table categoria(
+	id int primary key not null auto_increment,
+    nome varchar(50) not null
+);
+
+create table artigo(
+	id int primary key not null auto_increment,
+    titulo varchar(50) not null,
+	id_categoria int,
+    conteudo varchar(255) not null
+);
+
+create table usuario(
+	id int primary key not null auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    data_nascimento date not null,
+    cpf varchar(11) not null,
+    telefone varchar(15) not null
+);
+
+
